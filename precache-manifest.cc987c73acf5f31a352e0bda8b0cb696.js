427049fe85020801450a1a264b4259d5c7d33cf9
Gabriela Saldana <gabriela.saldana@wizeline.com>
2020-01-30 15:24:52 -0600

    Updates

---
+++ precache-manifest.cc987c73acf5f31a352e0bda8b0cb696.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a6820ac2d80cc35e30343c3e77ba57",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "bcad26c269694059b162",
+    "url": "/profile/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1e0b614a964b6666c307",
+    "url": "/profile/static/js/2.10801725.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/profile/static/js/2.10801725.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bcad26c269694059b162",
+    "url": "/profile/static/js/main.e5b6422f.chunk.js"
+  },
+  {
+    "revision": "b5a7d259590e62b51bcc",
+    "url": "/profile/static/js/runtime-main.3026efc4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
