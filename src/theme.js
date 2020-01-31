6fa847b283609b5f0c67e6f5244bf5459a172146
Gabriela Saldana <gabriela.saldana@wizeline.com>
2020-01-31 14:17:15 -0600

    one more test to deploy

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,16 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import { grey } from "@material-ui/core/colors";
+import { grey, deepOrange } from "@material-ui/core/colors";
 
-const textPrimary = grey["500"]; // #263238
+const textPrimary = grey["800"]; // #263238
+const orange = deepOrange["500"];// #ff5722
 
 const theme = createMuiTheme({
   palette: {
     primary: {
       main: textPrimary
+    },
+    secondary: {
+      main: orange
     }
   }
 });
