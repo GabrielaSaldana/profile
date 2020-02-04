017d805947226bb5fab792636845bd79b603d6b9
Gabriela Saldana <gabriela.saldana@wizeline.com>
2020-02-04 10:49:24 -0600

    adding color theming

--- src/theme.js
+++ src/theme.js
@@ -1,16 +1,47 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import { grey, deepOrange } from "@material-ui/core/colors";
-
-const textPrimary = grey["800"]; // #263238
-const orange = deepOrange["500"];// #ff5722
+import {
+  grey,
+  orange,
+  red,
+  green,
+  cyan,
+  pink,
+  indigo,
+  yellow,
+  deepPurple
+} from "@material-ui/core/colors";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: textPrimary
+      main: indigo["A200"] //#536dfe
     },
     secondary: {
-      main: orange
+      main: orange["A400"] //#ff9100
+    },
+    text: {
+      primary: grey["800"], // #424242
+      secondary: grey["600"] //#616161
+    }
+  },
+  extendedPalette: {
+    redContrast: {
+      main: red["400"] //#ef5350
+    },
+    cyanContrast: {
+      main: cyan["400"] //#26c6da
+    },
+    pinkContrast: {
+      main: pink["A100"] //#ff80ab
+    },
+    greenContrast: {
+      main: green["A400"] //#00e676 
+    },
+    yellowContrast: {
+      main: yellow["A700"] //#ffd600
+    },
+    purpleContrast: {
+      main: deepPurple["300"], //#9575cd
     }
   }
 });
