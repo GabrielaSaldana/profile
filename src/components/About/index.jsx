04de7aa7094b13c051373c731554aeb811a05d3e
Gabriela Saldana <gabriela.saldana@wizeline.com>
2020-02-06 14:11:25 -0600

    adding paper style to images

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 import { Typography } from "@material-ui/core";
 import Naruto from "../../images/naruto.jpg";
-import Systems from "../../images/systems.jpg";
+import Systems from "../../images/systems.png";
 import Plants from "../../images/plants.png";
 
 const useStyles = makeStyles(theme => ({
@@ -15,10 +16,17 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(1),
     textAlign: "justify"
   },
-  textImage: {
+  image: {
     width: "100%",
     marginTop: theme.spacing(2),
     borderRadius: "4%"
+  },
+  paper: {
+    padding: theme.spacing(1),
+    height: theme.spacing(34)
+  },
+  textBox: {
+    height: theme.spacing(12)
   }
 }));
 
@@ -29,28 +37,34 @@ function About() {
     <div id="about" className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={4}>
-          <Typography>
-            Many people know my addiction to anime, the japanese culture amaze
-            me, the order they have, teh automation they have in their public
-            services and their loyalty to their culture.
-          </Typography>
-          <img src={Naruto} className={classes.textImage} alt="Naruto" />
+          <Paper className={classes.paper}>
+            <Typography className={classes.textBox}>
+              Many people know my addiction to anime, the japanese culture amaze
+              me, the order they have, teh automation they have in their public
+              services and their loyalty to their culture.
+            </Typography>
+            <img src={Naruto} className={classes.image} alt="Naruto" />
+          </Paper>
         </Grid>
         <Grid item xs={4}>
-          <Typography>
-            I'm a Software Engineer by alma mater, but curious enough to be
-            interested in design, management, processes and the Software
-            environment as a whole. I'm amazed by the progress we as a human
-            species have mad so far in the recent years.
-          </Typography>
-          <img src={Systems} className={classes.textImage} alt="Systems" />
+          <Paper className={classes.paper}>
+            <Typography className={classes.textBox}>
+              I'm a Software Engineer by alma mater, but curious enough to be
+              interested in design, management, processes and the Software
+              environment as a whole. I'm amazed by the progress we as a human
+              species have mad so far in the recent years.
+            </Typography>
+            <img src={Systems} className={classes.image} alt="Systems" />
+          </Paper>
         </Grid>
         <Grid item xs={4}>
-          <Typography>
-            If you can take care of a plant, then, you can take care of
-            yourself. My hobby is to raise my plants.
-          </Typography>
-          <img src={Plants} className={classes.textImage} alt="Plants" />
+          <Paper className={classes.paper}>
+            <Typography className={classes.textBox}>
+              If you can take care of a plant, then, you can take care of
+              yourself. My hobby is to raise my plants.
+            </Typography>
+            <img src={Plants} className={classes.image} alt="Plants" />
+          </Paper>
         </Grid>
       </Grid>
     </div>
