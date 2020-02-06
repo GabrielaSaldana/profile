aabf22e749730ff77af3456aa027e8d036e52386
Gabriela Saldana <gabriela.saldana@wizeline.com>
2020-02-06 10:00:30 -0600

    Adding images that I like

--- src/components/Presentation/index.jsx
+++ src/components/Presentation/index.jsx
@@ -1,21 +1,66 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
+import TopView from "../../images/topview.jpg";
+import Palms from "../../images/palms.jpg";
+import Mountain from "../../images/mountain.jpg";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    color: theme.palette.text.primary,
-    border: "1px solid grey",
-    margin: theme.spacing(1)
-  }
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    transform: 'translateZ(0)',
+  },
+  title: {
+    color: theme.extendedPalette.greyContrast.main,
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
 }));
+
+const tileData = [
+  {
+    img: TopView,
+    title: "Nature is unbelievable"
+  },
+  {
+    img: Palms,
+    title: "Palms are great"
+  },
+  {
+    img: Mountain,
+    title: "Walk always right to the top"
+  }
+];
 
 function Presentation() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Typography variant="h1">Carousel with images</Typography>
+      <GridList className={classes.gridList} cols={2.5}>
+        {tileData.map(tile => (
+          <GridListTile key={tile.img}>
+            <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              classes={{
+                root: classes.titleBar,
+                title: classes.title
+              }}
+            />
+          </GridListTile>
+        ))}
+      </GridList>
     </div>
   );
 }
