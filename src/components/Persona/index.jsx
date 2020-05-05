642deaf0d612e2db497fb491ed727015ea0b78fc
Gabriela Saldana <gabriela.saldana@wizeline.com>
2020-05-05 16:09:32 -0500

    deleting unused img and adjusting text sizes

--- src/components/Persona/index.jsx
+++ src/components/Persona/index.jsx
@@ -13,10 +13,22 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2),
     fontSize: "1.1rem",
   },
-  title: {
+  titleBox: {
     textAlign: "left",
     marginBottom: theme.spacing(2),
   },
+  title: {
+    [theme.breakpoints.up("md")]: {
+      fontSize: "6rem",
+    },
+    fontSize: "2rem",
+  },
+  subtitle: {
+    [theme.breakpoints.up("md")]: {
+      fontSize: "2rem",
+    },
+    fontSize: "1.2rem"
+  }
 }));
 
 function Persona() {
@@ -24,30 +36,37 @@ function Persona() {
 
   return (
     <div className={classes.root}>
-      <div className={classes.title}>
-        <Typography style={{ fontSize: "6rem" }}>
+      <div className={classes.titleBox}>
+        <Typography className={classes.title}>
           Hello, I am Gabriela. I enjoy hepling people solving problems of all
           kinds trough my own learning.
           <span role="img" aria-label="nerd">
             🤓
           </span>
         </Typography>
-        <Typography variant="h1">
+        <Typography className={classes.subtitle}>
           Bachelor of Science degree in Computer Systems Engineering from ESCOM
           - Instituto Politecnico Nacional
         </Typography>
       </div>
       <Typography gutterBottom className={classes.description}>
-        <b style={{ color: "#F780AB"}}>I’m passionate about knowledge in all forms</b>, specially when you combine
-        technology and human behaviours. I like to design the way people and technology
-        interact improving their daily experiences. I'm such a inquisitive and
-        friendly person that wants to help people by collecting ideas from
-        everything around me, understanding their culture, pain points,
-        happiness and traditions wherever I am. My daily work includes empathy,
-        strategy, logic, strategy, and common sense and willness to help people get their goals.
-        I have a background in software engineering but I've been actively seeking new roles in my life.
-        <b style={{ color: "#F780AB" }}> I'm just a problem solver who likes knowing the big picture, holistic is
-        the only way to be inclusive from our own perspective.</b>
+        <b style={{ color: "#F780AB" }}>
+          I’m passionate about knowledge in all forms
+        </b>
+        , specially when you combine technology and human behaviours. I like to
+        design the way people and technology interact improving their daily
+        experiences. I'm such a inquisitive and friendly person that wants to
+        help people by collecting ideas from everything around me, understanding
+        their culture, pain points, happiness and traditions wherever I am. My
+        daily work includes empathy, strategy, logic, strategy, and common sense
+        and willness to help people get their goals. I have a background in
+        software engineering but I've been actively seeking new roles in my
+        life.
+        <b style={{ color: "#F780AB" }}>
+          {" "}
+          I'm just a problem solver who likes knowing the big picture, holistic
+          is the only way to be inclusive from our own perspective.
+        </b>
       </Typography>
     </div>
   );
