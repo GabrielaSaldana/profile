b66c94f32300166b5a35f9a9290433d3d2b9dc2d
Gabriela Saldana <gabriela.saldana@wizeline.com>
2020-05-06 09:43:58 -0500

    correcting writing

--- src/components/Persona/index.jsx
+++ src/components/Persona/index.jsx
@@ -38,7 +38,7 @@ function Persona() {
     <div className={classes.root}>
       <div className={classes.titleBox}>
         <Typography className={classes.title}>
-          Hello, I am Gabriela. I enjoy hepling people solving problems of all
+          Hello, I am Gabriela. I enjoy helping people solving problems of all
           kinds trough my own learning.
           <span role="img" aria-label="nerd">
             🤓
@@ -53,13 +53,12 @@ function Persona() {
         <b style={{ color: "#F780AB" }}>
           I’m passionate about knowledge in all forms
         </b>
-        , specially when you combine technology and human behaviours. I like to
-        design the way people and technology interact improving their daily
-        experiences. I'm such a inquisitive and friendly person that wants to
-        help people by collecting ideas from everything around me, understanding
-        their culture, pain points, happiness and traditions wherever I am. My
+        , especially when you combine technology and human behaviors. I like to design the way people
+        and technology interact improving their daily experiences. I'm such an inquisitive and friendly
+        person that wants to help people by collecting ideas from everything around me, understanding
+        their culture, pain points, happiness, and traditions wherever I am. My
         daily work includes empathy, strategy, logic, strategy, and common sense
-        and willness to help people get their goals. I have a background in
+        and willingness to help people get their goals. I have a background in
         software engineering but I've been actively seeking new roles in my
         life.
         <b style={{ color: "#F780AB" }}>
